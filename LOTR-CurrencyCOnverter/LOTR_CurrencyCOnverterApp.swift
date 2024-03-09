95462f15998372beee05b36bc151b5aec4e76926
Vishwas Sharma <ch0nky@protonmail.com>
2024-03-09 15:20:32 +0530

    Initial Commit

---
+++ LOTR-CurrencyCOnverter/LOTR_CurrencyCOnverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_CurrencyCOnverterApp.swift
+//  LOTR-CurrencyCOnverter
+//
+//  Created by Vishwas Sharma on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_CurrencyCOnverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
